6e4abe9b89871e6599b0422cf1c6ccad366901d0
Akbar Pratama <51158308+AkbarPratama01@users.noreply.github.com>
2024-12-07 20:33:00 +0700

    Revert "Initial commit"
    This reverts commit 524edc89ca1ebe427a0550c4311c71b70995777a.

--- src/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Controllers\HomeController;
-
-// Example of routing definition
-return [
-    '/' => [HomeController::class, 'index'],
-    '/about' => [HomeController::class, 'about'],
-];
